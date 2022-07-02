237975de8220f1e0cff8c804fbcf2effc2c568f3
perry1224 <perry.w1224@gmail.com>
2022-07-01 23:03:20 -0400

    updated files

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 
 export class MovieView extends React.Component {
 
+  keypressCallback(event) {
+    console.log(event.key);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keypress', this.keypressCallback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
+
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} crossOrigin='anonymous'/>
+          <img src={movie.ImagePath} crossOrigin='anonymous' height= '200' />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
